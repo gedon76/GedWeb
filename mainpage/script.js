9b95f4e2a0d35d8bc944c324102ad17f42cbe0a4
Gedon76 <82772622+GEDON6676@users.noreply.github.com>
2023-07-19 23:49:01 +0300

    Delete mainpage directory

--- mainpage/script.js
+++
@@ -1,8 +0,0 @@
-const searchForm = document.getElementById('search-form');
-      const searchInput = document.getElementById('search-input');
-      searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const query = searchInput.value;
-        const searchUrl = 'https://duckduckgo.com?q=' + query;
-        window.location.href = searchUrl;
-      });
